63d70ee8eeb6a4d3a3b7592320f22868716ede94
arburhan <buthan190629@outlook.com>
2022-08-02 20:33:02 +0600

    work on banner

--- src/Components/Pages/Banner/Banner.js
+++ src/Components/Pages/Banner/Banner.js
@@ -2,11 +2,19 @@ import React from 'react';
 import Typed from 'react-typed';
 import windowsIcon from '../../../Media/Images/icons8-windows-10-48.svg';
 import './Banner.css';
+import okkhorImage from '../../../Media/Images/okkhor.png';
+
+
 const Banner = () => {
     return (
         <div className='py-8 BannerBG' >
+            <div className='flex justify-center my-5 md:my-0' >
+                <figure>
+                    <img className='w-[50%] md:w-[20%] mx-auto' src={okkhorImage} alt="" />
+                </figure>
+            </div>
             <div className='text-center px-5 md:px-56'>
-                <h1 className='text-3xl md:text-7xl' >Speech to text. Text without
+                <h1 className='text-3xl md:text-6xl' >Speech to text. Text without
                     <div>
                         <Typed
                             strings={['typing...']}
@@ -16,14 +24,14 @@ const Banner = () => {
                         />
                     </div>
                 </h1>
-                <p className='py-6 md:py-8 text-xl' >
+                <p className='py-6 md:py-8 text-md md:text-xl' >
                     If you're tired
                     <Typed
                         strings={["If you're tired of typing, or just don't have the time to do it, Okkhor Voice Typing is the solution. With this easy-to-use app, you can speak and get text in real time. You can use Voice To Text app to create text using any language in the world. Long text,  messages, emails or reports on the go without interrupting your workflow."]}
                         typeSpeed={40}
                     />
                 </p>
-                <button className='btn my-2' >
+                <button className='btn my-2 text-white' >
                     <img className='px-2 ' src={windowsIcon} alt="" />  Download Now
                 </button>
             </div>
